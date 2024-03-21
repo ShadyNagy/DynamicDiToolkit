49c4da5a792499728c0493145fb542a3b6916176
ShadyNagy <shadynagi@gmail.com>
2024-03-22 00:37:48 +0200

    DynamicDiToolkit update the add services.

--- src/DynamicDiToolkit/Extensions/ServiceCollectionExtensions.cs
+++ src/DynamicDiToolkit/Extensions/ServiceCollectionExtensions.cs
@@ -5,21 +5,21 @@ using Microsoft.Extensions.DependencyInjection;
 namespace DynamicDiToolkit.Extensions;
 public static class ServiceCollectionExtensions
 {
-	public static IServiceCollection AddSingletonArdalisSpecificationRepositoryFactoryServices(this IServiceCollection services)
+	public static IServiceCollection AddSingletonDynamicDiToolkitServices(this IServiceCollection services)
 	{
 		services.AddSingleton<IArdalisSpecificationRepositoryFactory, ArdalisSpecificationRepositoryFactory>();
 
 		return services;
 	}
 
-	public static IServiceCollection AddScopedArdalisSpecificationRepositoryFactoryServices(this IServiceCollection services)
+	public static IServiceCollection AddScopedDynamicDiToolkitServices(this IServiceCollection services)
 	{
 		services.AddScoped<IArdalisSpecificationRepositoryFactory, ArdalisSpecificationRepositoryFactory>();
 
 		return services;
 	}
 
-	public static IServiceCollection AddTransientArdalisSpecificationRepositoryFactoryServices(this IServiceCollection services)
+	public static IServiceCollection AddTransientDynamicDiToolkitServices(this IServiceCollection services)
 	{
 		services.AddTransient<IArdalisSpecificationRepositoryFactory, ArdalisSpecificationRepositoryFactory>();
 
